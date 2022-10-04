50ca7915ad6fe6e815b7b1f2a39ed957ade9abf2
vikas <kuchipudivikas@gmail.com>
2022-10-04 17:11:03 +0530

    add loading screen

--- src/routes/interview/interview.component.jsx
+++ src/routes/interview/interview.component.jsx
@@ -1,16 +1,13 @@
 import React from 'react'
 import { useState, useEffect } from 'react'
 import { doc, setDoc } from "firebase/firestore";
-
 import { db } from '../../utils/firebase.utils'
 import { collection, getDocs } from 'firebase/firestore'
 import { python } from '../../utils/questions'
 import Parser from 'html-react-parser'
-import { Button } from 'react-bootstrap'
-import { Collapse, Container } from 'react-bootstrap';
-import Card from 'react-bootstrap/Card';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import { Collapse, Container, Button, Card, Row, Col, Spinner } from 'react-bootstrap';
+
+
 
 
 
@@ -20,6 +17,8 @@ const Interview = () => {
     const questionsCollectionsRef = collection(db, "questions")
     const [open, setOpen] = useState(false);
     const [currentIndex, setCurrentIndex] = useState(0)
+    const [loading, setLoading] = useState(true)
+    let count = 0
 
     useEffect(() => {
 
@@ -36,11 +35,12 @@ const Interview = () => {
                 selectedTopics.push({ ...element.data() })
             });
 
-            const count = Math.floor(20 / selectedTopics.length)
+            count = Math.floor(20 / selectedTopics.length)
             selectedTopics.forEach(topic => {
                 const { questions } = topic;
                 for (let i = 0; i < count; i++) {
                     const index = Math.floor(Math.random() * questions.length)
+
                     const curr = questions[index];
                     const question = Parser(curr.question)
                     const answer = Parser(curr.answer)
@@ -50,6 +50,7 @@ const Interview = () => {
 
 
             setFinalInterviewQuestions(interviewQuestions)
+            setLoading(false)
         }
 
         const tester = async () => {
@@ -68,54 +69,67 @@ const Interview = () => {
 
 
     return (
-        <div>
+        <div className="border d-flex align-items-center justify-content-center">
 
-            <Container className='mt-15'>
-                <Row>
+            {
+                loading ? (
 
-                    <Col xs={9}><Card>
+                    <div className="border d-flex align-items-center justify-content-center">
+                        <Spinner animation="border" />
 
-
-                        <Card.Header>Featured</Card.Header>
-                        <Card.Body>
-                            <Card.Title>Special title treatment</Card.Title>
-                            <Card.Text>
-                                {finalInterviewQuestions[currentIndex].question}
-                            </Card.Text>
-                            <Button
-                                onClick={() => setOpen(!open)}
-                                aria-controls="example-collapse-text"
-                                aria-expanded={open}
-                            >
-                                show answer
-                            </Button>
-                            <Button
-                                onClick={() => {
-                                    setCurrentIndex(index => index + 1)
-                                    setOpen(false)
-                                }}
-                                aria-controls="example-collapse-text"
-                            // aria-expanded={open}
-                            >
-                                Next question
-                            </Button>
-                            <Collapse in={open}>
-                                <div id="example-collapse-text">
-                                    {finalInterviewQuestions[currentIndex].answer}
-                                </div>
-                            </Collapse>
-                        </Card.Body>
-
-                    </Card>
-                    </Col>
-
-                    <Col >user video</Col>
-                </Row>
-
-            </Container>
+                    </div>
 
 
-        </div>
+                ) : (<Container className='mt-15'>
+
+                    <Row>
+
+                        <Col xs={9}><Card>
+
+
+                            <Card.Header>Featured</Card.Header>
+                            <Card.Body>
+                                <Card.Title>Special title treatment</Card.Title>
+                                <Card.Text>
+                                    {finalInterviewQuestions[currentIndex].question}
+                                </Card.Text>
+                                <Button
+                                    onClick={() => setOpen(!open)}
+                                    aria-controls="example-collapse-text"
+                                    aria-expanded={open}
+                                >
+                                    show answer
+                                </Button>
+                                <Button
+                                    onClick={() => {
+                                        setCurrentIndex(index => index + 1)
+                                        setOpen(false)
+                                    }}
+                                    aria-controls="example-collapse-text"
+                                >
+                                    Next question
+                                </Button>
+                                <Collapse in={open}>
+                                    <div id="example-collapse-text">
+                                        {finalInterviewQuestions[currentIndex].answer}
+                                    </div>
+                                </Collapse>
+                            </Card.Body>
+
+                        </Card >
+                        </Col >
+
+                        <Col >user video</Col>
+                    </Row >
+
+                </Container >)
+
+            }
+
+
+
+
+        </div >
     )
 }
 
